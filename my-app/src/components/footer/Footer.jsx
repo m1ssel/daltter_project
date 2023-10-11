b2f1cb6759888a4f7d5910ca476028fd31cafba0
m1ssel <artemaz665@gmail.com>
2023-10-11 20:06:27 +0200

    changes

--- my-app/src/components/footer/Footer.jsx
+++ my-app/src/components/footer/Footer.jsx
@@ -1,7 +1,22 @@
+import './footer.css';
 
 function Footer() {
     return (
-        <div></div>
+        <div className='footer'>
+            <div className="logo-footer">
+                <img src="/src/img/logo-black.png" alt="logo"/>
+                <p>daltter</p>
+            </div>
+            <div className='footer-links'>
+                <a href="#">Register</a>
+                <div className='line'></div>
+                <a href="#">Terms of service</a>
+                <div className='line'></div>
+                <a href="#">Privacy policy</a>
+                <div className='line'></div>
+                <a href="#">Login</a>
+            </div>
+        </div>
     );
 }
 
--- my-app/src/components/main/Main.jsx
+++ my-app/src/components/main/Main.jsx
@@ -21,7 +21,7 @@ function Main() {
                     <img src="/src/img/Second-leftservice-left.png" alt="image" />
                 </div>
                 <div className="second-right">
-                    <h1 className='second-main'>Our service is fully <br/> automated</h1>
+                    <h1 className='second-main'>Our <span>service</span> is fully <br/> automated</h1>
                     <p className='second-text'>All you need is just few clicks - and your clients <br/> will be in your hands.
                     Forget about tens different <br/> services for one ad.</p>
                     <div className="second-btn"><a href="#" className='second-try-button'>Try it now</a></div>
@@ -29,6 +29,16 @@ function Main() {
             </div>
             <div className="main-third">
                 <h1>We have several different <br/> plans to suit all customers:</h1>
+            </div>
+            <div className="main-fourth">
+                <h1>Not sure if our service is right for you?</h1>
+                <div className='leave-email'>
+                    <p>Leave us your email and we <br/> will write to you:</p>
+                    <div className='email-input'>
+                        <input type="text" className='email-address' placeholder='Email Address'/>
+                        <a href="#">></a>
+                    </div>
+                </div>
             </div>
         </div>
     );
